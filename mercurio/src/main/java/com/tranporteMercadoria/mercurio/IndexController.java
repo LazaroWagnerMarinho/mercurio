afee42324824593a02be3fd03dd305a2e46031a1
lazaromarinho <lazarowagnermarinho@gmail.com>
2019-04-06 15:30:17 -0300

    testando merge

--- mercurio/src/main/java/com/tranporteMercadoria/mercurio/IndexController.java
+++ mercurio/src/main/java/com/tranporteMercadoria/mercurio/IndexController.java
@@ -1,4 +1,4 @@
-package com.tranporteMercadoria.mercurio.controllers;
+package com.tranporteMercadoria.mercurio;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,5 +9,10 @@ public class IndexController {
 	@RequestMapping("/mercurio")
 	public String index() {
 		return "homeMercurio";
+	}
+	
+	@RequestMapping("/index")
+	public String index1() {
+		return "pessoas/formPessoas";
 	}
 }
--- mercurio/src/main/java/com/tranporteMercadoria/mercurio/controllers/PessoasController.java
+++ mercurio/src/main/java/com/tranporteMercadoria/mercurio/controllers/PessoasController.java
@@ -1,14 +1,29 @@
 package com.tranporteMercadoria.mercurio.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.tranporteMercadoria.mercurio.models.cadastroPessoas;
+import com.tranporteMercadoria.mercurio.repository.PessoasRepository;
 
 @Controller
 public class PessoasController {
 	
-	@RequestMapping("/cadastrarPessoas")
+	@Autowired
+	private PessoasRepository pr;
+	
+	@RequestMapping(value="/cadastrarPessoas", method=RequestMethod.GET)
 	public String form() {
 		return "pessoas/formPessoas";
 	}
-
+	
+	@RequestMapping(value="/cadastrarPessoas", method=RequestMethod.POST)
+	public String form(cadastroPessoas pessoas) {
+		
+		pr.save(pessoas);
+		
+		return "redirect:/cadastrarPessoas";
+	}
 }

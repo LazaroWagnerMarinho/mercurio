1e74847a461b9d6279bd7f8f282995ae5f2d7616
Lazaro Marinho <lazaro.marinho@radixeng.com.br>
2019-04-10 14:54:11 -0300

    configurando heroku

---
+++ mercurio/src/main/java/com/tranporteMercadoria/mercurio/models/enderecoPessoas.java
@@ -0,0 +1,16 @@
+/*
+ * package com.tranporteMercadoria.mercurio.models;
+ * 
+ * import java.io.Serializable;
+ * 
+ * import javax.persistence.Entity; import javax.persistence.Table;
+ * 
+ * @Entity
+ * 
+ * @Table(name="TB_ENDERECO_PESSOAS") public class enderecoPessoas implements
+ * Serializable{ private static final long serialVersionUID = 1L;
+ * 
+ * 
+ * 
+ * }
+ */
